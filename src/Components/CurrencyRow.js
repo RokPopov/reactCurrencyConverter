5812d868f2fa635801ba96ef58e86dcb12a06710
rokpopov <rokpopov90@gmail.com>
2021-05-16 22:19:27 +0200

    hooking onChange event to onChangeCurrency function in CorrencyRow component, and passing it the value of the selected currency option

--- src/Components/CurrencyRow.js
+++ src/Components/CurrencyRow.js
@@ -6,7 +6,7 @@ export default function CurrencyRow(props) {
   return (
     <div>
       <input type="number" className="input"/>
-      <select value={props.selectedCurrency}>
+      <select value={props.selectedCurrency} onChange={props.onChangeCurrency}>
         {props.currencyOptions.map((opt,i)=> (
           <option key={i} value={opt}>{opt}</option>
         ))}        
