f22b97f7cfdb3539bf33207e29c4d80e72335f57
rokpopov <rokpopov90@gmail.com>
2021-05-16 20:05:35 +0200

    creatie and style components, fetch currency converter api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React, { useEffect } from 'react';
 import './App.css';
+import CurrencyRow from './Components/CurrencyRow.js';
+
+const BASE_URL = 'https://api.exchangeratesapi.io/v1/latest';
+
+
 
 function App() {
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [])
   return (
-    <h1
-    style={{ textAlign:"center" }}
-    >Currency Converter</h1>
+    <div>
+    <h1>Currency Converter</h1>
+    <CurrencyRow />
+    <div className="equals">=</div>
+    <CurrencyRow />
+    </div>
   );
 }
 
